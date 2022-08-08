1fcebb3058a7b77eba682bd796b5183f67022576
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-08-09 00:18:59 +0100

    form_app injection

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('apply/', include('form_app.urls'))
 
 
 
