5e5bea6da27b2bc776c90812c5ac271e6e6976b3
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-08-08 22:58:51 +0100

    another one

--- website/views.py
+++ website/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from .models import Carousel
+
 
 # Create your views here.
 
 def home(request):
 
-    general_info = Carousel.objects.all()
-    context = {
-        'general_info': general_info,
-    }
 
-    return render(request, 'website/index.html', context)
+    return render(request, 'website/index.html')
