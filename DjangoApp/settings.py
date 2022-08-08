c944c77c239a2fa4982d0607f9cfb3d7bd190392
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-08-08 23:33:21 +0100

    testing new app

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yszd010jus=4_^u+&$c)d+%3^mp48erx4dafdm+1o4548fzyro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['176.58.124.157']
+ALLOWED_HOSTS = ['176.58.124.157', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
 
 
 
