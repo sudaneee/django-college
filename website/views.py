3c097fb100cb217cae4392cb979ee639d2f998df
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-08-08 23:49:14 +0100

    tester5

--- website/views.py
+++ website/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'website/index.html')
+    return HttpResponse('Hello Abbas')
