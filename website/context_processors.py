bb7ab3ef101a3c5a2651e522901909f504a14a8c
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-08-11 21:18:39 +0100

    commit

--- website/context_processors.py
+++ website/context_processors.py
@@ -1,22 +1,24 @@
 from turtle import title
-from .models import GeneralInformation, Carousel, Picture, Paragraph, Course, Staff
+from .models import GeneralInformation, Carousel, Picture, Paragraph, Course, Staff, Blog
 
 
 def data_processor(request):
     query = GeneralInformation.objects.all()[0]
-    # carousel = Carousel.objects.all()
-    # about1 = Picture.objects.get(title="about1")
-    # choose1 = Picture.objects.get(title="choose1")
-    # choose_p = Paragraph.objects.get(title="choose_p")
-    # courses = Course.objects.all()
-    # staffs = Staff.objects.all()
+    carousel = Carousel.objects.all()
+    about1 = Picture.objects.get(title="about1")
+    choose1 = Picture.objects.get(title="choose1")
+    choose_p = Paragraph.objects.get(title="choose_p")
+    courses = Course.objects.all()
+    staffs = Staff.objects.all()
+    blogs = Blog.objects.all()
     return {
         'data' : query,
-        # 'car1': carousel[0],
-        # 'car2': carousel[1],
-        # 'about1': about1,
-        # 'choose1': choose1,
-        # 'choose_p': choose_p,
-        # 'courses': courses,
-        # 'staffs': staffs,
+        'car1': carousel[0],
+        'car2': carousel[1],
+        'about1': about1,
+        'choose1': choose1,
+        'choose_p': choose_p,
+        'courses': courses,
+        'staffs': staffs,
+        'blogs': blogs,
     }
